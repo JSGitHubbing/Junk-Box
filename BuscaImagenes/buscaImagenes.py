1cb4cc02b46b10b9e02484d94614b1e8f8241831
Suso <osuso@tutanota.com>
2021-02-24 16:38:16 +0100

    Implementado buscaImagenes.py. Añadido .png a .gitignore

---
+++ BuscaImagenes/buscaImagenes.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from PIL import Image
+import shutil, os
+Image.MAX_IMAGE_PIXELS = 3299452380
+
+def archivosEnRuta(rutaDondeGuardar, rutaDondeMirar):
+
+    listaElementos = rutaDondeMirar.iterdir()
+    extensiones = ['.jpg', '.png']
+
+    for elem in listaElementos:
+        if elem.is_dir():
+            archivosEnRuta(rutaDondeGuardar, elem)
+        elif elem.suffix in extensiones:
+            img = Image.open(elem)
+            if img._size == (64, 64):
+                c = rutaDondeGuardar.joinpath("MisFotitos")
+                c.mkdir(0o777, False, True)
+                shutil.copy(elem, c)
+
+def clasificarFotos(ruta):
+    rutaDondeGuardar = Path(ruta)
+    rutaDondeMirar = Path(ruta)
+    archivosEnRuta(rutaDondeGuardar, rutaDondeMirar)
+
+clasificarFotos('/home/platonium/Imágenes')
